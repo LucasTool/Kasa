2c657f776ca27b89069b3fe8565d45511e39ab7e
Lucas Toulet <lucas.toulet@gmail.com>
2025-09-05 16:25:02 +0200

    ajout collapse

--- frontend/src/Components/Collapse.jsx
+++ frontend/src/Components/Collapse.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ArrowUp from '../assets/angle-small-up.png';
+import ArrowDown from '../assets/angle-small-down.png';
+import './Collapse.scss'
 
-function Collapse() {
-  return (
-    <div>Collapse</div>
-  )
-}
+const Collapse = ({title, text}) => {
+    const [isOpen, setIsOpen] = useState(false)
 
-export default Collapse
+    return (
+        <article className='collapseContainer'>
+            <div className='collapseTitle' >
+                <p>{title}</p>
+                <img
+                   className="arrow"
+                   onClick={() => setIsOpen(!isOpen)}
+                   src={isOpen ? ArrowUp : ArrowDown}
+                   alt={isOpen ? "arrow up" : "arrow down"}
+                   role="button"
+                   />
+            </div>
+            <div className={`collapseText ${isOpen ? 'Out' : ''}`}>
+                {text}
+                </div>
+        </article>
+       
+    )
+};
+
+export default Collapse;
