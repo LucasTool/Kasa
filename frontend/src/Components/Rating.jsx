c57f5147c94015e7980b3bcd3f11433153801574
Lucas Toulet <lucas.toulet@gmail.com>
2025-09-09 00:58:00 +0200

    ajout rating

--- frontend/src/Components/Rating.jsx
+++ frontend/src/Components/Rating.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Rating.scss'
 
-function Rating() {
-  return (
-    <div>Rating</div>
-  )
-}
 
-export default Rating
+const Rating = ({ stars }) => {
+    const FullStar = <i data-testid="full-star" className="fa-solid fa-star fa-xl"></i>
+    const EmptyStar = <i data-testid="empty-star" className="fa-solid fa-star fa-xl empty"></i>
+
+    return (
+        <div className='rating'>
+            {Array.from({ length: 5 }, (_, index) => (
+                <span className='star' key={index}>
+                    {index < stars ? FullStar : EmptyStar}
+                </span>
+            ))}
+        </div>
+    );
+};
+
+export default Rating;
